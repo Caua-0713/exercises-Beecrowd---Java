dacb4b618438c04f5b964518b0acb16816dc91f0
CauaLima-ui <caualim001@gmail.com>
2026-08-31 23:21:48 -0300

    Exe1001

---
+++ Exe1001/Exe1001/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+
+        Scanner sc = new Scanner(System.in);
+        int a, b, sum;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        sum = a + b;
+
+System.out.println("X = " + sum);
+
+
+    }
+}
